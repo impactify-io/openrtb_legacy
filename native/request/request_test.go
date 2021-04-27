38e60d24268385272babbd44fa9b16eefed47519
Thomas <thomas.dseao@gmail.com>
2021-04-27 11:00:05 +0200

    Update request_test.go

--- native/request/request_test.go
+++ native/request/request_test.go
@@ -5,7 +5,7 @@ import (
 	"io/ioutil"
 	"path/filepath"
 
-	. "github.com/mxmCherry/openrtb/native/request"
+	. "github.com/thomasdseao/openrtb/native/request"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/ginkgo/extensions/table"
